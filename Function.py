1830b2aa53cd1271da35ba735e9cc14ba5a6c35f
Nimesh <110623261+DhananjayaYN@users.noreply.github.com>
2022-09-11 14:53:17 +0530

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 19 22:19:36 2022
+
+@author: Laptop Outlet
+"""
+def greet (name):
+    "Greet a person with name"
+    print("Hello!,")
+    print(name)
+    return
+greet("Nimal")
+greet("kamal")
+    
+    
+dir(0)
+    
