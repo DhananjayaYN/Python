99ab7844ca8d2bf485ff19134b92229147f0fe83
Nimesh <110623261+DhananjayaYN@users.noreply.github.com>
2022-09-11 14:52:33 +0530

    Add files via upload

---
+++ Conditional Execution.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 09:33:43 2022
+
+@author: Laptop Outlet
+"""
+
+print("If statement")
+
+time=10
+if time<12:
+    print("Good Morning!")
+if time>12:
+    print("Good Afternoon!")
+print("Have a great day!")    
+
+print("If-else statement")
+
+time=14
+if time<12:
+    print("Good Morning!")
+else:
+    print("Good Afernoon!")
+    
+print("If-elif-else statement")
+
+x=59
+if x>=75:
+    print("A")
+elif x>=65:
+    print ("B")
+elif x>=50:
+    print("C")
+elif x>=35:
+    print("S")
+else:
+    print("F")    
+    
