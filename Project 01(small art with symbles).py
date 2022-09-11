35bed3e1d54e44495d98a39db4b3fdb32fc6e9ab
Nimesh <110623261+DhananjayaYN@users.noreply.github.com>
2022-09-11 14:50:36 +0530

    Add files via upload

---
+++ Project 01(small art with symbles).py
@@ -0,0 +1,30 @@
+i=1
+for x in range(0,2):
+    for y in range(0,15):
+        print('%',end='')
+    print('')
+    
+while i<8:
+    print('#'*i)
+    i+=1
+while i>0:
+    print('@'*i)
+    i-=1
+    
+for x in range(0,2):
+    for y in range(0,15):
+        print('%',end='')
+    print('')
+    
+i=1    
+while i<8:
+    print('$'*i) 
+    i+=1
+while i>0 :
+    print('&'*i)
+    i-=1
+
+for x in range(0,2):
+    for y in range(0,15):
+        print('%',end='')
+    print('')         
