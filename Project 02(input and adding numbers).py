f0da702167b4dd6b968d6669e71ae4b065e53f86
Nimesh <110623261+DhananjayaYN@users.noreply.github.com>
2022-09-17 23:34:49 +0530

    Add files via upload

---
+++ Project 02(input and adding numbers).py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Adding five numbers what user input
+
+# In[2]:
+
+
+a=1;
+while(a<6):
+    b=input("Enter number "+str(a)+" ;")
+    if(a==1):
+       sum= int(b)
+    else:
+       sum+=int(b)
+    a+=1
+print(sum)        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
